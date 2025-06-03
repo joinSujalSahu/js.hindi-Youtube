7f7c305739ea99fa7214d2f508c70f0e91899c4e
Sujal sahu <167052814+sujallsahu@users.noreply.github.com>
2025-06-03 04:02:42 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =144553
+let accountEmail ="Sujal@gmail.com"
+var accountPass ="222333"
+accountCity = "Delhi"
+
+// accountId=2
+
+accountEmail ="hfg@gmail.com"
+accountPass ="12"
+accountCity ="Bihar"
+
+console.table([accountEmail,accountId,accountPass,accountCity])
+
+
+// console.log(accountId);
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all js code as a new version 
+
+// alert(3+3)  we are using node.js not browser 
+
+console.log(3+3)
+console.log("Sujal")
+
+
+/*number = 2 ^ 53
+bigint = for large numbers
+string = "assembly of characters"
+boolean = true or false
+null = stand alone value (type = object)
+undefined = kindda place holder for a value which is not defined yet (type = undefined)
+symbol = uniquness*/
+
+
+console.log(typeof null) //typeof null is object because null is a object 
+console.log(typeof undefined); //typeof undefined is undefined 
+
