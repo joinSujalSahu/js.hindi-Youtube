c20b201424bd13821c81c6d9ace212be773e27e1
Sujal sahu <167052814+sujallsahu@users.noreply.github.com>
2025-06-06 16:55:20 +0000

    datatypes-Summary

---
+++ datatypes-Summary.js
@@ -0,0 +1,35 @@
+// primitive datatype 
+// 7 types :- String , Number , Boolean , Null , Undefined , Symbol , BigInt
+let email; //undefined 
+let number = null 
+let a=100;
+let b=101.11
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+// console.log(id==anotherId);
+
+
+const bigNum=2444521455n;
+
+// Reference type also called as (Non primitive) datatype
+// Array ,Objects , Functions 
+
+let hero =["Superman", "Batman"]
+// console.log(hero)
+
+let obj1 ={
+    name :"sujal",
+    id :101
+}
+console.log(obj1);
+
+
+
+const myFunction = function(){
+    console.log("Hello World!");
+    
+}
+
+
