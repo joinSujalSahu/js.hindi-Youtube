1b249b82bddf955a4cae08f0178566fdb95cfe3a
Sujal sahu <167052814+sujallsahu@users.noreply.github.com>
2025-06-06 15:05:05 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc"
-console.log(typeof(score));
-console.log(typeof score);
+// console.log(typeof(score));
+// console.log(typeof score);
 
 let valueInNumber = Number(score) //similar for all datatypes 
-console.log(typeof valueInNumber );
-console.log(valueInNumber);
+// console.log(typeof valueInNumber );
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -12,4 +12,40 @@ console.log(valueInNumber);
 // null => 0
 // undefined => NaN
 // true => 1. false => 0
+
+
+
+// ********************* Operations ******************************
+
+
+let value = 7
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Sujal"
+let str2 = " Sahu"
+let str3 = str1+str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+1+"2");
+
+let num1 ,num2, num3
+num1=num2=num3=2+2
+// console.log(num1, num2,num3);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
 
