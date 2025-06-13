6407027963887c31bb4c94ecb32d7ccd2b16271a
Sujal sahu <167052814+joinSujalSahu@users.noreply.github.com>
2025-06-13 08:58:11 +0000

    json talk

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -17,10 +17,10 @@ const JsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
+console.log(JsUser)
 // console.log(JsUser["email"])
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+//console.log(JsUser[mySym])
 
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
@@ -28,11 +28,11 @@ JsUser.email = "hitesh@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
-    console.log("Hello JS user");
+   // console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    //console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
