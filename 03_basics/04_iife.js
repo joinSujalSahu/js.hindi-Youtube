38e86de8ca1b3595229d2d363fff12c8984ef4a9
Sujal sahu <167052814+joinSujalSahu@users.noreply.github.com>
2025-06-21 04:59:01 +0000

    iife concepts

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,11 +1,18 @@
 // Immediately Invoked Function Expressions (IIFE)
 
 
-(function chai(){
-    // named IIFE
-    console.log(`DB CONNECTED`);
+(function chai (){
+    console.log(`Db connected`);
+    
 })();
 
+
+// arrow functions with iife
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('sujal')
+} )('sujal');
+
+
+((user1,user2) =>{
+    console.log(`this db is connectes by two users user one is ${user1}, and user two is ${user2}`);
+})('sujal','ritwik');
