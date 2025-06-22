9975b185b0349384e2a3d7b93ce62276463a345e
Sujal sahu <167052814+joinSujalSahu@users.noreply.github.com>
2025-06-22 09:01:13 +0000

    switch in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 13;
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+         case 2:
+        console.log("feb");
+        
+        break;
+         case 3:
+        console.log("march");
+        
+        break;
+
+    default:
+        console.log("Invalid input");
+        
+        break;
+}
